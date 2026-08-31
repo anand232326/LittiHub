f650b553040714df24733ccc750d344e541bd4be
anand232326 <a8362k@gmail.com>
2026-08-31 22:14:33 +0530

    restaurant get_by_id

--- app/routers/v1/restaurants.py
+++ app/routers/v1/restaurants.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from app.controllers.restaurant_controller import restaurant_controller
 from app.dependencies.auth import require_role
 from app.core.enums import UserRole
@@ -20,4 +20,23 @@ async def create_restaurant(restaurant_data: RestaurantCreate,
 current_user: User = Depends(require_role(UserRole.ADMIN)),):
     return await restaurant_controller.create(
         restaurant_data
+    )
+
+
+@router.get("/{restaurant_id}",response_model=RestaurantResponse)
+async def get_restaurant(restaurant_id:str):
+    restaurant = await restaurant_controller.get_by_id(restaurant_id)
+    if restaurant is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Restaurant with id '{restaurant_id}' not found",
+        )
+        
+    return restaurant
+
+
+@router.get("/",response_model=list[RestaurantResponse],)
+async def get_restaurants(city: str | None = None,):
+    return await restaurant_controller.get_all(
+        city=city
     )
