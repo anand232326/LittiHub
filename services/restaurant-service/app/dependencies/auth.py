ae068c9e55d07359cd674c9c869bebb1651bbae2
anand232326 <a8362k@gmail.com>
2026-09-21 21:24:43 +0530

    updated restaurant dependency

--- services/restaurant-service/app/dependencies/auth.py
+++ services/restaurant-service/app/dependencies/auth.py
@@ -1,45 +1,54 @@
+from typing import Callable
 from fastapi import Depends
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
-from jose import JWTError,jwt
-from typing import Callable
+from jose import JWTError, jwt
+
 from app.core.config import Config
-from app.core.exceptions import AppException,PermissionDeniedError
+from app.core.exceptions import AppException, PermissionDeniedError
+
+security = HTTPBearer()
 
 
-security=HTTPBearer()
-
-async def get_current_user(credentials:HTTPAuthorizationCredentials=Depends(security))->dict:
-    token=credentials.credentials
+async def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+) -> dict:
+    token = credentials.credentials
 
     try:
-        payload=jwt.decode(
+        # Wrap Config.ALGORITHM inside a list []
+        algorithms = (
+            [Config.ALGORITHM]
+            if isinstance(Config.ALGORITHM, str)
+            else Config.ALGORITHM
+        )
+
+        payload = jwt.decode(
             token,
             Config.SECRET_KEY,
-            algorithms=[Config.ALGORITHM]
+            algorithms=algorithms,
         )
-    except JWTError as exc:
+    except JWTError:
         raise AppException(
             "invalid or expire access token",
             status_code=401,
-        )    
+        )
 
-    user_id=payload.get("sub")
+    user_id = payload.get("sub")
 
     if not user_id:
         raise AppException(
             "invalid access token",
             status_code=401,
         )
+
     return payload
 
 
-
-
-
-def require_role(*allowed_roles:str)->Callable:
+def require_role(*allowed_roles: str) -> Callable:
     async def role_checker(
-            current_user:dict=Depends(get_current_user),)->dict:
-        user_role=current_user.get("role")
+        current_user: dict = Depends(get_current_user),
+    ) -> dict:
+        user_role = current_user.get("role")
 
         if user_role not in allowed_roles:
             raise PermissionDeniedError(
